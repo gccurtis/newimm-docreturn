9d0c764d4776738dff0e3ed81c0b815e85d0633c
mimira04 <joshua@wittkoski.com>
2024-10-17 13:28:19 -0400

    Fixed data entry classes and added test classes

--- src/main/java/edu/gmu/cs321/ImmReqForm.java
+++ src/main/java/edu/gmu/cs321/ImmReqForm.java
@@ -1,3 +1,4 @@
+package edu.gmu.cs321;
 
 public class ImmReqForm {
 
---
+++ src/test/java/edu/gmu/cs321/ImmReqValidatorTest.java
@@ -0,0 +1,8 @@
+package edu.gmu.cs321;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ImmReqValidatorTest {
+
+}

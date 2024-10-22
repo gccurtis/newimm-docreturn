1ef987f8b44526482149a698bd3d366c961daf51
mimira04 <joshua@wittkoski.com>
2024-10-21 21:27:56 -0400

    Added comments to tests

--- src/test/java/edu/gmu/cs321/DENewImmReqTest.java
+++ src/test/java/edu/gmu/cs321/DENewImmReqTest.java
@@ -5,9 +5,13 @@ import org.junit.Test;
 import java.time.LocalDate;
 
 public class DENewImmReqTest {
+    /**
+     * Checks that newImmReqForm creates an ImmReqForm
+     */
     @Test
     public void checkNewImmReqForm() {
         DENewImmReq testDE = new DENewImmReq();
-        assertTrue(testDE != null);
+        ImmReqForm testForm = testDE.newImmReqForm(null, null, null, null, 0, null, null, null, null);
+        assertTrue(testForm != null);
     }
 }
--- src/test/java/edu/gmu/cs321/ImmReqFormTest.java
+++ src/test/java/edu/gmu/cs321/ImmReqFormTest.java
@@ -8,6 +8,9 @@ import edu.gmu.cs321.ImmReqForm.ImmReqValidator;
 import java.time.LocalDate;
 
 public class ImmReqFormTest {
+    /**
+     * Checks that all fields are validated with validateNewImmReq
+     */
     @Test
     public void checkValidateNewImmReq() {
         ImmReqForm testForm = new ImmReqForm("first", "middle", "last", LocalDate.of(2000, 1, 1), 1, "test@email.com",
@@ -15,6 +18,9 @@ public class ImmReqFormTest {
         assertTrue(testForm.validateNewImmReq());
     }
 
+    /**
+     * Checks that saveImmReqForm saves the ImmReqForm to the database
+     */
     @Test
     public void checkSaveImmReqForm() {
         ImmReqForm testForm = new ImmReqForm("first", "middle", "last", LocalDate.of(2000, 1, 1), 1, "test@email.com",
@@ -22,6 +28,9 @@ public class ImmReqFormTest {
         assertTrue(testForm.saveImmReqForm());
     }
 
+    /**
+     * Checks that addWFItem adds the ImmReqForm to the Workflow
+     */
     @Test
     public void checkAddWFItem() {
         ImmReqForm testForm = new ImmReqForm("first", "middle", "last", LocalDate.of(2000, 1, 1), 1, "test@email.com",
@@ -29,54 +38,81 @@ public class ImmReqFormTest {
         assertTrue(testForm.addWFItem());
     }
 
+    /**
+     * Checks that the firstName field is validated with validateWord
+     */
     @Test
     public void checkValidateFirstName() {
         ImmReqForm testForm = new ImmReqForm("first", null, null, null, 0, null, null, null, null);
         assertTrue(ImmReqValidator.validateWord(testForm.getFirstName()));
     }
 
+    /**
+     * Checks that the middleName field is validated with validateWord
+     */
     @Test
     public void checkValidateMiddleName() {
         ImmReqForm testForm = new ImmReqForm(null, "middle", null, null, 0, null, null, null, null);
         assertTrue(ImmReqValidator.validateWord(testForm.getMiddleName()));
     }
 
+    /**
+     * Checks that the lastName field is validated with validateWord
+     */
     @Test
     public void checkValidateLastName() {
         ImmReqForm testForm = new ImmReqForm(null, null, "last", null, 0, null, null, null, null);
         assertTrue(ImmReqValidator.validateWord(testForm.getLastName()));
     }
 
+    /**
+     * Checks that the dateOfBirth field is validated with validateDOB
+     */
     @Test
     public void checkValidateDOB() {
         ImmReqForm testForm = new ImmReqForm(null, null, null, LocalDate.of(2000, 1, 1), 0, null, null, null, null);
         assertTrue(ImmReqValidator.validateDOB(testForm.getDateOfBirth()));
     }
 
+    /**
+     * Checks that the ID field is validated with validateID
+     */
     @Test
     public void checkValidateID() {
         ImmReqForm testForm = new ImmReqForm(null, null, null, null, 1, null, null, null, null);
         assertTrue(ImmReqValidator.validateID(testForm.getID()));
     }
 
+    /**
+     * Checks that the email field is validated with validateEmail
+     */
     @Test
     public void checkValidateEmail() {
         ImmReqForm testForm = new ImmReqForm(null, null, null, null, 0, "test@email.com", null, null, null);
         assertTrue(ImmReqValidator.validateEmail(testForm.getEmail()));
     }
 
+    /**
+     * Checks that the race field is validated with validateWord
+     */
     @Test
     public void checkValidateRace() {
         ImmReqForm testForm = new ImmReqForm(null, null, null, null, 0, null, "race", null, null);
         assertTrue(ImmReqValidator.validateWord(testForm.getRace()));
     }
 
+    /**
+     * Checks that the gender field is validated with validateWord
+     */
     @Test
     public void checkValidateGender() {
         ImmReqForm testForm = new ImmReqForm(null, null, null, null, 0, null, null, "gender", null);
         assertTrue(ImmReqValidator.validateWord(testForm.getGender()));
     }
 
+    /**
+     * Checks that the requestedDoc field is validated with validateWord
+     */
     @Test
     public void checkValidateDocument() {
         ImmReqForm testForm = new ImmReqForm(null, null, null, null, 0, null, null, null, "document");

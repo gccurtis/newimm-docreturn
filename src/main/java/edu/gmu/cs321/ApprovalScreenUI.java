78ddcba816f370e8cd088b0e27201bab66b584d7
gccurtis <gccurtis001@gmail.com>
2024-11-04 15:02:11 -0500

    Created mock data for approval screen and added a reject button

--- src/main/java/edu/gmu/cs321/ApprovalScreenUI.java
+++ src/main/java/edu/gmu/cs321/ApprovalScreenUI.java
@@ -24,6 +24,9 @@ public class ApprovalScreenUI {
     private Stage stage;
 
     public ApprovalScreenUI() {
+	//Create a function to pass next ImmReqForm
+	ImmReqForm immData = new ImmReqForm("Azek","Mirus","Lyrenidae",LocalDate.of(2000,1,1),1,"gccurtis001@gmail.com","ambiguous","ambiguous","Form1");
+	
         stage = new Stage();
         stage.setTitle("Document Request Approval");
         GridPane grid = new GridPane();
@@ -38,18 +41,30 @@ public class ApprovalScreenUI {
 
         Label firstName = new Label("First Name:");
         grid.add(firstName, 0, 1);
+        Label firstNameVal = new Label(immData.getFirstName());
+        grid.add(firstNameVal, 1, 1);
 
         Label middleName = new Label("Middle Name:");
         grid.add(middleName, 0, 2);
+        Label middleNameVal = new Label(immData.getMiddleName());
+        grid.add(middleNameVal, 1, 2);
 
         Label lastName = new Label("Last Name:");
         grid.add(lastName, 0, 3);
+        Label lastNameVal = new Label(immData.getLastName());
+        grid.add(lastNameVal, 1, 3);
 
         Button btn = new Button("Approve");
         HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BASELINE_RIGHT);
+        hbBtn.setAlignment(Pos.BASELINE_LEFT);
         hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
+        grid.add(hbBtn, 0, 4);
+
+        Button btn2 = new Button("Reject");
+        HBox hbBtn2 = new HBox(10);
+        hbBtn2.setAlignment(Pos.BASELINE_RIGHT);
+        hbBtn2.getChildren().add(btn2);
+        grid.add(hbBtn2, 1, 4);
 
         final Text actiontarget = new Text();
         grid.add(actiontarget, 0, 6);
@@ -65,73 +80,16 @@ public class ApprovalScreenUI {
             }
         });
 
-        Scene scene = new Scene(grid, 300, 275);
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Reject button pressed");
+            }
+        });
+
+        Scene scene = new Scene(grid, 400, 300);
         stage.setScene(scene);
         stage.show();
-    }
-
-    /**
-     * @param word word is the word being validated
-     * @return returns true if word isn't null or an empty string, false
-     *         otherwise
-     */
-    public static boolean validateWord(String word) {
-        if ((word == null) || (word.length() < 1))
-            return false;
-        return true;
-    }
-
-    /**
-     * @param dateOfBirth dayOfBirth is the date of birth being validated
-     * @return returns true if dayOfBirth is within the range of 1-31, false
-     *         otherwise
-     */
-    public static boolean validateBirthDay(int dayOfBirth) {
-        if ((dayOfBirth < 1) || (dayOfBirth > 31))
-            return false;
-        return true;
-    }
-
-    /**
-     * @param monthOfBirth monthOfBirth is the date of birth being validated
-     * @return returns true if monthOfBirth is within the range of 1-12, false
-     *         otherwise
-     */
-    public static boolean validateBirthMonth(int monthOfBirth) {
-        if ((monthOfBirth < 1) || (monthOfBirth > 12))
-            return false;
-        return true;
-    }
-
-    /**
-     * @param yearOfBirth yearOfBirth is the date of birth being validated
-     * @return returns true if yearOfBirth is within the range of 1920-2024, false
-     *         otherwise
-     */
-    public static boolean validateBirthYear(int yearOfBirth) {
-        if ((yearOfBirth < 1920) || (yearOfBirth > 2024))
-            return false;
-        return true;
-    }
-
-    /**
-     * @param ID ID is the ID being validated
-     * @return returns true if ID exists and is greater than 0, false otherwise
-     */
-    public static boolean validateID(int ID) {
-        if (ID < 0)
-            return false;
-        return true;
-    }
-
-    /**
-     * @param email email is the email being validated
-     * @return returns true if email isn't null and contains the "@" and "."
-     *         characters, false otherwise
-     */
-    public static boolean validateEmail(String email) {
-        if ((email == null) || (!email.contains("@")) || (!email.contains(".")))
-            return false;
-        return true;
     }
 }

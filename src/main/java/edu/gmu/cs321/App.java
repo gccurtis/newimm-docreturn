42719bcaa2c5188af84a55706ac9f21126be4f0a
mimira04 <joshua@wittkoski.com>
2024-11-07 15:53:31 -0500

    Comment

--- src/main/java/edu/gmu/cs321/App.java
+++ src/main/java/edu/gmu/cs321/App.java
@@ -6,29 +6,30 @@ import java.util.Scanner;
 
 /**
  * run using 'mvn clean javafx:run'
+ * Dumb, random comment
  */
 public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-	String screenType = System.getProperty("screen.type");
-	ApprovalScreenUI testa;
-        DENewImmReq testde;
-	if (screenType.equals("E"))
-        	testde = new DENewImmReq();
-	if (screenType.equals("A"))
-		testa= new ApprovalScreenUI();
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		String screenType = System.getProperty("screen.type");
+		ApprovalScreenUI testa;
+		DENewImmReq testde;
+		if (screenType.equals("E"))
+			testde = new DENewImmReq();
+		if (screenType.equals("A"))
+			testa = new ApprovalScreenUI();
+	}
 
-    public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("Enter the corresponding number for the following:\n1. Data Entry\n2. Review\n3. Approval");
-	int choice = scanner.nextInt();
-	System.out.println("You entered: " + choice);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the corresponding number for the following:\n1. Data Entry\n2. Review\n3. Approval");
+		int choice = scanner.nextInt();
+		System.out.println("You entered: " + choice);
 
-	if (choice == 1)
-		System.setProperty("screen.type", "E");
-	if(choice == 3)
-		System.setProperty("screen.type","A");
-        launch(args);
-    }
+		if (choice == 1)
+			System.setProperty("screen.type", "E");
+		if (choice == 3)
+			System.setProperty("screen.type", "A");
+		launch(args);
+	}
 }

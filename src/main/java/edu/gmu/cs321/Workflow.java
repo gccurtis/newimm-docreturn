bfdd62fd87418d26b718c2ed1e6e3e085d8e73da
gccurtis <gccurtis001@gmail.com>
2024-11-15 11:09:42 -0500

    Added workflow functionality to add to database

--- src/main/java/edu/gmu/cs321/Workflow.java
+++ src/main/java/edu/gmu/cs321/Workflow.java
@@ -2,6 +2,10 @@ package edu.gmu.cs321;
 
 import java.util.Queue;
 import java.util.PriorityQueue;	
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Workflow {
 
@@ -42,6 +46,35 @@ public class Workflow {
 
 	public WorkflowItem getNextWorkflowItem() {
 		return this.queue.poll();
+	}
+
+	public void addWorkflowItemToDB(WorkflowItem wfi) {
+		String url = "jdbc:mysql://localhost:3306/testdb";
+		String user = "cs321";
+		String password = "password";
+
+		String sqlInsertQuery = "INSERT INTO users (id, firstName, middleName, lastName, email, race, gender, requestedDoc) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+		try (Connection conn = DriverManager.getConnection(url,user,password); PreparedStatement pstmt = conn.prepareStatement(sqlInsertQuery)) {
+			pstmt.setString(2,wfi.getForm().getFirstName());
+			pstmt.setString(3,wfi.getForm().getMiddleName());
+			pstmt.setString(4,wfi.getForm().getLastName());
+			//pstmt.setString(5,"");
+			pstmt.setString(5,wfi.getForm().getEmail());
+			pstmt.setString(6,wfi.getForm().getRace());
+			pstmt.setString(7,wfi.getForm().getGender());
+			pstmt.setString(8,wfi.getForm().getRequestedDoc());
+			pstmt.setInt(1, wfi.getForm().getID());
+
+			int rowsAffected = pstmt.executeUpdate();
+
+			if (rowsAffected > 0) {
+				System.out.println("New User Added");
+			}
+		} catch (SQLException e) {
+			System.out.println("ERROR");
+			e.printStackTrace();
+		}
 	}
 
 }
